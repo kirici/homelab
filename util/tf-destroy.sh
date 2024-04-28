21d5dc393f01eba31e100fad20d27b604bd327d7
kirici <74684021+kirici@users.noreply.github.com>
2024-04-28 13:53:35 +0200

    feat(util): conveniency tf scripts to apply and destroy

---
+++ util/tf-destroy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+
+# Global vars
+SCRIPTPATH="$(dirname "$(realpath "$0")")"
+TF_PATH=$(realpath "${SCRIPTPATH}"/../terraform/infra)
+
+id="$(id -u)"
+if [ "$id" != "0" ]; then
+  echo "Root required."
+  exit
+fi
+
+terraform -chdir="${TF_PATH}" destroy -auto-approve
