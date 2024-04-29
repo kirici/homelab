d1397d41b4cfeed53b687697d4d6d46188808923
kirici <74684021+kirici@users.noreply.github.com>
2024-04-29 20:48:38 +0200

    feat(util): retry until secret exists

--- util/argocd-initial-secret.sh
+++ util/argocd-initial-secret.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-kubectl -n argocd get secret argocd-initial-admin-secret -o go-template='{{.data.password|base64decode}}'
+until kubectl -n argocd get secret argocd-initial-admin-secret -o go-template='{{.data.password|base64decode}}';
+do
+  printf "."
+  sleep 1
+done
