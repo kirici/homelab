da025142f25c0800406cf65a653c45a68b82c143
kirici <74684021+kirici@users.noreply.github.com>
2025-06-07 00:23:10 +0200

    fix(util): run script the same regardless of current working dir

--- util/k0sctl-apply.sh
+++ util/k0sctl-apply.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 # Global vars
 SCRIPTPATH="$(dirname "$(realpath "$0")")"
 
-pushd ${SCRIPTPATH}
+pushd ${SCRIPTPATH}/..
 yq 'explode(.) | select(di==1)' ./k0sctl.yaml.template > ./k0sctl.yaml
 k0sctl apply --config ./k0sctl.yaml
+popd
