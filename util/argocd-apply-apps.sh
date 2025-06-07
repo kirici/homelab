983796f98760576d331b5cd79cccd356c17df51b
kirici <74684021+kirici@users.noreply.github.com>
2025-06-08 01:08:50 +0200

    mv: change project layout

--- util/argocd-apply-apps.sh
+++ util/argocd-apply-apps.sh
@@ -4,4 +4,4 @@ set -eu
 # Global vars
 SCRIPTPATH="$(dirname "$(realpath "$0")")"
 
-kubectl apply --recursive -f "$(realpath "${SCRIPTPATH}"/../argocd/apps)"
+kubectl apply --recursive -f "$(realpath "${SCRIPTPATH}"/../k8s/argocd/apps)"
