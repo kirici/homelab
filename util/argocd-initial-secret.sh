57ea935d64488a41ec3c1b8ea6428afa01ed2fcc
kirici <74684021+kirici@users.noreply.github.com>
2025-08-03 00:19:49 +0200

    feat(util): use kubectl's wait as readiness probe

--- util/argocd-initial-secret.sh
+++ util/argocd-initial-secret.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-until kubectl -n argocd get secret argocd-initial-admin-secret &>/dev/null;
-do
-  sleep 1
-done
-
+kubectl -n argocd wait --for=create --timeout=240s secret/argocd-initial-admin-secret
 kubectl -n argocd get secret argocd-initial-admin-secret -o go-template='{{.data.password|base64decode}}'
--- util/argocd-portfwd.sh
+++ util/argocd-portfwd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -eu
 
+kubectl -n argocd wait --for=create --timeout=240s secret/argocd-initial-admin-secret
 nohup kubectl -n argocd port-forward services/argocd-server 8080:443 &>/dev/null &
